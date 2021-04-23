4cef439168b405cb7ba7b146973d68653c927c61
victor.ojeda <ojedavictor80@gmail.com>
2021-04-22 21:10:14 -0400

    Se realizó la implementación del crud de tareas con la conexión a lowdb

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,21 +1,35 @@
+const { getConnection } = require('../database');
+const { v4 } = require('uuid');
 const getTasks = (req, res) => {
-    res.send('getTasks');
+    const tasks = getConnection().get('tasks').value();
+    res.json(tasks);
 };
 
 const getTaskById = (req, res) => {
-    res.send('getTaskById');
+    const task = getConnection().get('tasks').find({ id: req.params.id }).value();
+    res.json(task);
 };
 
 const createTask = (req, res) => {
-    res.send('createTask');
+    const newTask = {
+        id: v4(),
+        name: req.body.name,
+        description: req.body.description
+    }
+    getConnection().get('tasks').push(newTask).write();
+    res.send(newTask);
 };
 
-const updateTask = (req, res) => {
-    res.send('updateTask');
+const updateTask = async (req, res) => {
+    const result = await getConnection().get('tasks').find({ id: req.params.id })
+        .assign(req.body)
+        .write();
+    res.json(result);
 };
 
 const deleteTask = (req, res) => {
-    res.send('deleteTask');
+    const result = getConnection().get('tasks').find({ id: req.params.id }).write();
+    res.json(result);
 };
 
 module.exports = { getTasks, getTaskById, createTask, updateTask, deleteTask }
